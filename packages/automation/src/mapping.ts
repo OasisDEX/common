df595cfaa43b39bbb11db3d051f202494c6a956c
Adam Skrodzki <adam@oazoapps.com>
2022-04-27 15:21:46 +0200

    new mappings

--- packages/automation/src/mapping.ts
+++ packages/automation/src/mapping.ts
@@ -11,6 +11,9 @@ export const commandAddressMapping: Record<number, Record<string, CommandContrac
     '0xd0ca9883e4918894dd517847eb3673d656ec9f2d': { type: CommandContractType.CloseCommand },
     '0x31285a87fb70a62b5aaa43199e53221c197e1e3f': { type: CommandContractType.CloseCommand },
   },
+  [EthereumNetwork.MAINNET]: {
+    '0xa553c3f4e65a1fc951b236142c1f69c1bca5bf2b': { type: CommandContractType.CloseCommand },
+  },
 };
 
 export const defaultCommandTypeMapping = {
@@ -46,6 +49,7 @@ export function getCommandContractInfo(address: string, network: number): Comman
   const lowercaseAddress = address.toLowerCase();
   const mappingForNetwork = commandAddressMapping[network as EthereumNetwork];
   if (!(lowercaseAddress in mappingForNetwork)) {
+    console.log("mappingForNetwork", mappingForNetwork, lowercaseAddress);
     throw new Error(`Command address ${lowercaseAddress} for network ${network} not found.`);
   }
 
