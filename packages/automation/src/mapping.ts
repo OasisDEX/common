5af010b5870fb9454b9d6d68c157dadf36e34a72
Adam Skrodzki <adam@oazoapps.com>
2022-07-06 15:47:22 +0200

    update

--- packages/automation/src/mapping.ts
+++ packages/automation/src/mapping.ts
@@ -11,8 +11,10 @@ export const commandAddressMapping: Record<number, Record<string, CommandContrac
     '0xd0ca9883e4918894dd517847eb3673d656ec9f2d': { type: CommandContractType.CloseCommand },
     '0x31285a87fb70a62b5aaa43199e53221c197e1e3f': { type: CommandContractType.CloseCommand },
     '0x7c86781a95b7e55e6c2f7297ae6773e1dbceab13': { type: CommandContractType.BasicBuyCommand },
+    '0xe3ae7218d8e4a482e212ef1cbf2fcd0fb9882cc7': { type: CommandContractType.BasicBuyCommand },
     '0x000000000000000000000000000000000000dead': { type: CommandContractType.BasicSellCommand },
     '0xd4f94e013c7f47b989ea79c6527e065c027794c7': { type: CommandContractType.BasicSellCommand },
+    '0x6f878d8eb84e48da49900a6392b8f9ed262a50d7': { type: CommandContractType.BasicSellCommand },
   },
   [EthereumNetwork.MAINNET]: {
     '0xa553c3f4e65a1fc951b236142c1f69c1bca5bf2b': { type: CommandContractType.CloseCommand },
