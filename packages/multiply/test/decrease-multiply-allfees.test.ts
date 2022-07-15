9c9344ea39e59ca9b0a204ea2cefd5b83276f8be
Adam Skrodzki <adam@oazoapps.com>
2022-07-15 07:34:17 +0200

    lint & format

--- packages/multiply/test/decrease-multiply-allfees.test.ts
+++ packages/multiply/test/decrease-multiply-allfees.test.ts
@@ -5,36 +5,36 @@ import { BigNumber } from 'bignumber.js';
 import { getMultiplyParams } from './../src/index';
 import { DesiredCDPState, MarketParams, VaultInfo } from '../src/internal/types';
 _chai.should();
-const one = new BigNumber(1);
-const zero = new BigNumber(0);
 
-let vaultCollRatio = (vaultInfo:VaultInfo,marketParams:MarketParams)=>{
-  return vaultInfo.currentCollateral.multipliedBy(marketParams.oraclePrice).dividedBy(vaultInfo.currentDebt);
+function vaultCollRatio(vaultInfo: VaultInfo, marketParams: MarketParams) {
+  return vaultInfo.currentCollateral
+    .multipliedBy(marketParams.oraclePrice)
+    .dividedBy(vaultInfo.currentDebt);
 }
-let marketParams: MarketParams;
-let vaultInfo: VaultInfo;
-let desiredCollRatio:number;
 
-marketParams = new MarketParams({
+const marketParams = new MarketParams({
   marketPrice: 2000,
   oraclePrice: 2200,
   FF: 0.0,
   OF: 0.002,
   slippage: 0.02,
 });
-vaultInfo = new VaultInfo(100000, 100, 1.5);
-desiredCollRatio = 5;
+const vaultInfo = new VaultInfo(100000, 100, 1.5);
+const desiredCollRatio = 5;
 
-describe.only(`decrease multiple, from ${vaultCollRatio(vaultInfo, marketParams).toNumber()} to ${desiredCollRatio}`,  () => {
-
-    it(`should calculate amounts that leads to ${desiredCollRatio} coll ratio`, async () => {
-        const desiredCdpState = new DesiredCDPState(new BigNumber(desiredCollRatio), 0, 0, 0, 0);
-        const retVal = getMultiplyParams(marketParams, vaultInfo, desiredCdpState, false);
-        const oracleValueOfNewVaultCollateral = retVal.collateralDelta.plus(vaultInfo.currentCollateral).multipliedBy(marketParams.oraclePrice);
-        const newVaultDebtAmount = retVal.debtDelta.plus(vaultInfo.currentDebt);
-        const newVaultCollRatio = oracleValueOfNewVaultCollateral.dividedBy(newVaultDebtAmount);
-        expect(newVaultCollRatio.toNumber()).to.be.greaterThan(desiredCollRatio * 0.9999);
-        expect(newVaultCollRatio.toNumber()).to.be.lessThan(desiredCollRatio * 1.0001);
-      });
-
-    });
+describe.only(`decrease multiple, from ${vaultCollRatio(
+  vaultInfo,
+  marketParams,
+).toNumber()} to ${desiredCollRatio}`, () => {
+  it(`should calculate amounts that leads to ${desiredCollRatio} coll ratio`, async () => {
+    const desiredCdpState = new DesiredCDPState(new BigNumber(desiredCollRatio), 0, 0, 0, 0);
+    const retVal = getMultiplyParams(marketParams, vaultInfo, desiredCdpState, false);
+    const oracleValueOfNewVaultCollateral = retVal.collateralDelta
+      .plus(vaultInfo.currentCollateral)
+      .multipliedBy(marketParams.oraclePrice);
+    const newVaultDebtAmount = retVal.debtDelta.plus(vaultInfo.currentDebt);
+    const newVaultCollRatio = oracleValueOfNewVaultCollateral.dividedBy(newVaultDebtAmount);
+    expect(newVaultCollRatio.toNumber()).to.be.greaterThan(desiredCollRatio * 0.9999);
+    expect(newVaultCollRatio.toNumber()).to.be.lessThan(desiredCollRatio * 1.0001);
+  });
+});
