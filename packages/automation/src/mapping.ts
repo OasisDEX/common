766193fddbd53cb743a08bd59bded5b9a339a83a
johnnieskywalker <lwolski0@gmail.com>
2022-10-07 12:56:59 +0200

    rm signature

--- packages/automation/src/mapping.ts
+++ packages/automation/src/mapping.ts
@@ -35,7 +35,6 @@ export const commandAddressMapping: Record<
       },
       '0xc3c42b67e4cbd6226b743352144d4b285c851f6e': {
         type: CommandContractType.AutoTakeProfitCommand,
-        overwrite: ['uint256', 'uint16', 'uint256', 'uint32'],
       },
     },
     [EthereumNetwork.MAINNET]: {
