ff8463f143d9ea7ba926e2554e858c7e97785e47
halaprix <panicz.klos@gmail.com>
2023-09-14 15:14:45 +0200

    chore: add spark sl address to automaiton

--- packages/automation/src/mapping.ts
+++ packages/automation/src/mapping.ts
@@ -160,6 +160,9 @@ export const commandAddressMapping: Record<
       '0xDC1c84Aac43F21f103e9bD0B091a1B5cc6433554': {
         type: CommandContractType.AaveStopLossCommandV2,
       },
+      '0xc49e905346bC68BdfB46ED1E46E0804ffDC4458a': {
+        type: CommandContractType.SparkStopLossCommandV2,
+      },
     },
   }).map(([network, mapping]) => [
     network,
